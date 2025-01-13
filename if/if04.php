400fec990ade35f5789ab4ba79ab9f4dc75ff860
anggun-khowatim <anggun.khowatim34@smk.belajar.id>
2025-01-13 09:15:24 +0700

    Create if04.php

---
+++ if/if04.php
@@ -0,0 +1,21 @@
+<?php
+$nilai = 85;
+
+if($nilai<60){
+    echo "Nilai : $nilai\n";
+    echo "Grade : E";
+}elseif($nilai<70){
+    echo "Nilai : $nilai\n";
+    echo "Grade : D";
+}elseif($nilai<80){
+    echo "Nilai : $nilai\n";
+    echo "Grade : C";
+}elseif($nilai<90){
+    echo "Nilai : $nilai\n";
+    echo "Grade : B";
+}elseif($nilai<=100){
+    echo "Nilai : A";
+}else{
+    echo "maaf nilai tidak sah";
+}
+?> 
